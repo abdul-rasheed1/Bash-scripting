d637c003d00f5a3386870444227f63b6d5758497
abdul-rasheed1 <rasheedjawula18@gmail.com>
2024-02-08 13:58:38 +0000

    this was just a simple intro to bash while loops, its an infinte loop though :)

---
+++ chapter_11/simple-while.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#######################################
+#Author:Abdul Rasheed
+#Version:v1.0.0
+#Date:2/8/2024
+#Description: implementing a simple never ending while loop
+#Usage: ./simple-while.sh
+#####################################
+#Infinite while loop
+
+while true;do
+	echo "Hello"
+	sleep 2 #waits for 3 secs
+done
